bf7e7b43da0f3d0e81a049aac475efe908b03530
qomal16019 <abdirahmanqomal16@gmail.com>
2020-05-19 17:58:05 +0300

    my exercises

---
+++ exercise2.py
@@ -0,0 +1,9 @@
+#concatenation
+first_name = "John"
+last_name = "Smith"
+age=45
+full_name = first_name + last_name
+print(full_name)
+#.dotformatting
+
+print("First Name: {} Last Name: {} Age:{} ".format(first_name,last_name,age))
---
+++ exercise3.py
@@ -0,0 +1,8 @@
+#ask user for their first name,lastname,location,year of birth
+#output the user input using string concatenation
+first_name=input("enter the firstname: ")
+last_name=input("enter the lastname: ")
+location=input("enter the location:")
+year_of_birth=input("enter the year of birth:")
+sum=first_name+ " " +last_name+location+year_of_birth
+print(sum)
